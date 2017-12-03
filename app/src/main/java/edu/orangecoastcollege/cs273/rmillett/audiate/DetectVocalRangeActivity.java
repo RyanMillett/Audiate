2ec862dd2a3ab397479921f9e5110b31c46b4b05
ryanmillett <rmillett@student.cccd.edu>
2017-12-02 21:52:58 -0800

    stubbed out VocalRangeDetection activity

--- app/src/main/java/edu/orangecoastcollege/cs273/rmillett/audiate/DetectVocalRangeActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/rmillett/audiate/DetectVocalRangeActivity.java
@@ -2,19 +2,56 @@ package edu.orangecoastcollege.cs273.rmillett.audiate;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class DetectVocalRangeActivity extends AppCompatActivity {
 
-    private PitchDetector mPitchDetector;
+    private double mHighNoteFreqHz;
+    private double mLowNoteFreqHz;
 
-    private TextView mPitchClass;
+    private double[] mNoteFreqs; // collect held note variance to calculate average
+    private double mFreqAvg; // average frequency based on values in mNoteFreqs array
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detect_vocal_range);
 
-        mPitchDetector = new PitchDetector(this, this);
+        PitchDetector pitchDetector = new PitchDetector(this, this);
+
+        ImageView pitchDetectImageView = findViewById(R.id.vocalRangeResultsImageView);
+        TextView highNoteTextView = findViewById(R.id.highNoteResultTextView);
+        TextView lowNoteTextView = findViewById(R.id.lowNoteResultTextView);
+
+        highNoteTextView.setText("");
+        lowNoteTextView.setText("");
+    }
+
+    /**
+     *
+     *
+     * @param view
+     */
+    public void detectPitchHandler(View view) {
+        // TODO: this method
+        // determine which button is selected (high/low)
+            // LONGPRESS: launch pitch detector, change button
+                // save and display results
+            // RELEASE: stop pitch detector, revert button
+    }
+
+    /**
+     *
+     *
+     * @param view
+     */
+    public void confirmVocalRange(View view) {
+        // TODO: this method
+        // button disabled unless both high/low fields are not null
+        // take results, update current user profile
+        // Are you sure? Y/N pop-up or something
+        // launch View Profile activity
     }
 }
