cebbaa20189ba2652350bcc3618ea55e7d593850
ryanmillett <rmillett@student.cccd.edu>
2017-12-02 17:16:26 -0800

    merged selection menus into single generic template

--- app/src/main/java/edu/orangecoastcollege/cs273/rmillett/audiate/ExerciseSelectionMenuActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/rmillett/audiate/ExerciseSelectionMenuActivity.java
@@ -1,29 +1,28 @@
 package edu.orangecoastcollege.cs273.rmillett.audiate;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public class EarTrainingMenuActivity extends AppCompatActivity {
+public class ExerciseSelectionMenuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ear_training_menu);
+        setContentView(R.layout.activity_exercise_selection_menu);
 
         // This should create the return button
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
-    public void earTrainingExerciseSelectionHandler(View view) {
+    public void exerciseSelectionHandler(View view) {
         // TODO: this method
         // displays exercise options in a ListView based on selected button (intervals/chords/modeScales)
     }
 
     public void startActivity(View view) {
         // TODO: this method
-        // starts an ear training quiz activity based on selection
+        // starts an exercise activity based on selection
         // button is enabled only if a quiz type and option is selected
     }
 
